be555d166604b5b996ffbc2de9a5f4ec63d3c97a
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-19 22:39:48 +0700

    feat(main): add main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/FebryanHernanda/minitask-day1-week8-koda3/internal"
+
+func main() {
+	/* Task 1 */
+	// internal.Task1()
+
+	/* Task 2 */
+	// internal.Task2("admin", "admin12345")
+
+	/* Task 3 */
+	// internal.Task3()
+
+	/* Task 4 */
+	internal.Task4()
+
+	/* Task 5 */
+	// internal.Task5()
+}
